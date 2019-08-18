75fa2b08c3f06ff74d9f76127d9e7f26baa52d49
Anatoly Scherbakov <altaisoft@gmail.com>
2019-08-18 22:41:25 +0700

    Unit tests to be more granular

--- tests/test_multi_value.py
+++ tests/test_multi_value.py
@@ -4,17 +4,22 @@ from starlette_lambda.aws import LambdaFunction
 
 
 def test_multi_value():
-    scope = LambdaFunction(asgi=Starlette()).get_connection_scope(event={
-        'httpMethod': 'GET',
-        'path': '/foo',
-        'headers': {},
-        'requestContext': {},
+    query_string = LambdaFunction(asgi=Starlette()).get_query_string(event={
         'queryStringParameters': {'make': 'TOYOTA', 'zip_code': '65301'},
         'multiValueQueryStringParameters': {
-            'make': ['TOYOTA'],
             'zip_code': ['02368', '65301']
         }
-    }, context={})
+    })
 
-    assert scope['query_string'] == 'make=TOYOTA&zip_code=02368&zip_code=65301'
+    assert query_string == 'make=TOYOTA&zip_code=02368&zip_code=65301'
 
+
+def test_pseudo_multi_value():
+    query_string = LambdaFunction(asgi=Starlette()).get_query_string(event={
+        'queryStringParameters': {'make': 'TOYOTA', 'zip_code': '65301'},
+        'multiValueQueryStringParameters': {
+            'zip_code': ['02368']
+        }
+    })
+
+    assert query_string == 'make=TOYOTA&zip_code=02368'
